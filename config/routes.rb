c2aad4022fe599662ce683d2542384f29cd173fb
i-shintaro <s.iwashita@di-v.co.jp>
2023-09-24 22:58:53 +0900

    ルーティングの編集add

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
- get 'posts', to: 'posts#index'
+ get 'posts', to: 'posts#index' #github練習commit-1
  get 'posts/new' , to: 'posts#new'
  post 'posts' , to: 'posts#create'
 end
